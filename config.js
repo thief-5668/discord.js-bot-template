b5ea5809565ba84ff47c0d119ad82688735e0079
RjsCodes <105872600+RjsCodes@users.noreply.github.com>
2022-09-28 18:49:01 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  Prefix: "?",
+
+  Users: {
+    OWNERS: ["YOUR USER ID", "OTHER OWNER ID"]
+  },
+
+  Handlers: {
+    MONGO: "YOUR MONGO URI"
+  },
+
+  Client: {
+    TOKEN: "YOUR BOT TOKEN",
+    ID: "YOUR BOT ID"
+  }
+
+}
